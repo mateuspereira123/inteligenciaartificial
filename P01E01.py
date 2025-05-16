e95974ec53e161e81d3506814473858b48491144
mateuspereira123 <mateuspdsb@gmail.com>
2025-05-15 22:56:58 -0400

    Add files via upload

---
+++ P01E01.py
@@ -0,0 +1,9 @@
+"""
+Aluno: Mateus Pereira de Sá Barbosa
+Turma:ENGCO212N01
+
+1 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]
+"""
+
+num = float(input("Informe um numero: "))
+print("O numero informado foi: ",num)
