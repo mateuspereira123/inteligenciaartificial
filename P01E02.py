66fcfa8721ac839f43b1a5f02b030645bcd95fc0
Mateus Pereira de Sá Barbosa <mateuspdsb@gmail.com>
2025-05-15 23:15:34 -0400

    P01E02.py

--- P01E02.py
+++ P01E02.py
@@ -2,7 +2,7 @@
 Aluno: Mateus Pereira de Sá Barbosa
 Turma:ENGCO212N01
 
-1 - Faça um Programa que peça dois números e imprima a soma.
+2 - Faça um Programa que peça dois números e imprima a soma.
 """
 
 num1 = float(input("Digite o primeiro numero: "))
