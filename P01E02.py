a4e1ad1a96680fc76c4d69018cf2f3174b9a926c
Mateus Pereira de Sá Barbosa <mateuspdsb@gmail.com>
2025-05-15 23:12:02 -0400

    Add files via upload

---
+++ P01E02.py
@@ -0,0 +1,11 @@
+"""
+Aluno: Mateus Pereira de Sá Barbosa
+Turma:ENGCO212N01
+
+1 - Faça um Programa que peça dois números e imprima a soma.
+"""
+
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+soma = num1 + num2
+print("A soma dos numeros eh: ", soma)
