91086b8c952d460e73d27e0d18b3a2adf3f2af6d
Mateus Pereira de Sá Barbosa <mateuspdsb@gmail.com>
2025-06-03 23:41:03 -0400

    Add files via upload

---
+++ P01E05.py
@@ -0,0 +1,12 @@
+"""
+Aluno: Mateus Pereira de Sá Barbosa
+Turma:ENGCO212N01
+
+5 - Faça um Programa que converta metros para centímetros.
+"""
+
+metros = float(input("Digite o valor em metros: "))
+
+cent = metros * 100
+
+print("O valor em centímetros eh: ", cent, "centímetros")
