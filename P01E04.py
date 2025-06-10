6bb4a032383a180b912b5cbeff5642bd579bb5ac
Mateus Pereira de Sá Barbosa <mateuspdsb@gmail.com>
2025-06-09 22:24:35 -0400

    Delete P01E04.py

--- P01E04.py
+++
@@ -1,9 +0,0 @@
-"""
-Aluno: Mateus Pereira de Sá Barbosa
-Turma:ENGCO212N01
-
-Faça um Programa que converta metros para centímetros
-"""
-met = float(input("Digite o valor em metros para ser convertido: "))
-cent = met*100
-print("O resultado da conversão de metros em centimetros eh: ", cent,"centímetros")
